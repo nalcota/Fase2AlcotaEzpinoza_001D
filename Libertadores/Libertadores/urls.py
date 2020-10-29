826ca48c2994b8792afe3564482d49f4c9b9b9e2
Nicolas Alcota <ni.alcota@alumnos.duoc.cl>
2020-10-29 10:13:48 -0300

    Corrigiendo

--- Libertadores/Libertadores/urls.py
+++ Libertadores/Libertadores/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from . import views
+from django.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('galeria/', views.galeria, name='galeria'),
-    path('form', views.form, name='form')
+    path('admin/', admin.site.urls),
+    path('copa/',include('copa.urls'))
+   
 
 ]
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
